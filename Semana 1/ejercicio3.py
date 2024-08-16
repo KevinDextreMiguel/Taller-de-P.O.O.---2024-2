dc06e76c9a215ff6213a796358e803afd5652376
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2024-08-16 18:50:15 -0500

    Update ejercicio3.py

--- Semana 1/ejercicio3.py
+++ Semana 1/ejercicio3.py
@@ -1,1 +1,16 @@
+codigo = int(input('Ingrese el còdigo: '))
+cantidad = int(input('Ingrese el cantidad: '))
 
+codigo = codigo % 10 
+precio = 0
+
+match codigo:
+  case 1: precio = 15.75
+  case 2: precio = 21
+  case 3: precio = 8.5
+  case 4: precio = 25
+  case 5: precio = 13.25
+  case _:print('Còdigo no vàlido')
+
+if codigo>0 and codigo<6:
+  print('Total a pagar s/',cantidad*precio,'.00')
