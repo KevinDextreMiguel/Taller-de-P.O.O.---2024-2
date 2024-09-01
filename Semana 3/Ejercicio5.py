637452268a78e45251eccff1a5876beee9c40d22
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2024-08-31 22:15:44 -0500

    Update Ejercicio5.py

--- Semana 3/Ejercicio5.py
+++ Semana 3/Ejercicio5.py
@@ -1,1 +1,11 @@
+def determinar_letra():
+  letra = input('Ingrese una letra: ')
+  if letra.isupper():
+    print('Es mayúscula')
+  elif letra.islower():
+    print('Es minúscula')
+  else:
+    print('No es una letra')
+  
 
+determinar_letra()
