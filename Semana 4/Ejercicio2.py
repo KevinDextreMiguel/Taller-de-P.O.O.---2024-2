69c5de12113fee53fa483f090e66caf9203726d9
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2024-09-07 23:11:59 -0500

    Update Ejercicio2.py

--- Semana 4/Ejercicio2.py
+++ Semana 4/Ejercicio2.py
@@ -1,57 +1,54 @@
 def ingresa_fecha():
   fecha = ''
-  while fecha.isdigit() == False or len(fecha) != 8:
-    fecha = input('Ingresa la fecha: ')
+  while len(fecha) != 8 or fecha.isdigit() == False:
+    fecha = input('Ingrese fecha: ')
   return fecha
 
-def decomponer_fecha():
-  fecha = ingresa_fecha()
-  AAAA = int(fecha[:4])
-  MM = int(fecha[4:6])
-  DD = int(fecha[6:8])
-  return AAAA,MM,DD
+def decomponer_fecha(fecha):
+  anio = int(fecha[0:4])
+  mes = int(fecha[4:6])
+  dia = int(fecha[6:8])
+  return anio,mes,dia
 
 
-def validar_fecha(AAAA,MM,DD):
-  if AAAA >= 1932 and AAAA<2025:
-    if MM >= 1 and MM <= 12:
-      if DD >= 1 and DD:
+def validar_rangos(anio,mes,dia):
+  if anio>= 1932 and anio<=2024:
+    if mes>=1 and mes <=12:
+      if dia>=1 and dia<=31:
         return True
   return False
 
-def verifica_fecha():
+
+def primer_filtro():
   while True:
-    AAAA,MM,DD = decomponer_fecha()
-    if validar_fecha(AAAA,MM,DD):
+    fecha = ingresa_fecha()
+    anio,mes,dia = decomponer_fecha(fecha)
+    if validar_rangos(anio,mes,dia):
       break
     else:
-      print(f'La fecha {AAAA}/{MM}/{DD} es incorrecta')
-  return AAAA,MM,DD
+      print(f'La fecha {anio}/{mes}/{dia} es incorrecta”')
+  return anio,mes,dia
 
-
-def retorna_dia_mayor(AAAA,MM):
-  if MM == 2:
-    if AAAA in bisiesto:
-      return diccionario[MM][1]
+def encontrar_maximo_dia(mes,anio):
+  if mes == 2:
+    if anio in bisiestos:
+      return diccionario[mes][1]
     else:
-      return diccionario[MM][0]
-  return diccionario[MM]
-
-
-def filtro_final():
-  AAAA,MM,DD = verifica_fecha()
-  dia_mayor = retorna_dia_mayor(AAAA,MM)
-  if DD <= dia_mayor:
-    print(f'La fecha {AAAA}/{MM}/{DD} es correcta')
+      return diccionario[mes][0]
   else:
-    print(f'“El día {DD} es mayor que el máximo día del mes {MM}”')
+    return diccionario[mes]
+
+def segundo_filtro():
+  anio,mes,dia = primer_filtro()
+  if dia <= encontrar_maximo_dia(mes,anio):
+    print(f'La fecha {anio}/{mes}/{dia} es correcta')
+  else:
+    print(f'“El día {dia} es mayor que el máximo día del mes {mes}”')
 
 
-
-diccionario = { 1:31,2:(28,29), 3:31,4:30,5:31,6:30,
-               7:31,8:31,9:30,10:31,11:30,12:31}
-bisiesto = []
+diccionario = {1:31,2:(28,29),3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
+bisiestos = []
 for i in range(1932,2025,4):
-  bisiesto.append(i)
+  bisiestos.append(i)
 
-filtro_final()
+segundo_filtro()
