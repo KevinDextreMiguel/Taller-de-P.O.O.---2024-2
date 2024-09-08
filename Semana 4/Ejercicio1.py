4a0f36234829ce60cdde619372ab080cb4657b0f
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2024-09-07 23:29:49 -0500

    Update Ejercicio1.py

--- Semana 4/Ejercicio1.py
+++ Semana 4/Ejercicio1.py
@@ -1,88 +1,92 @@
-def ingresa_transporte():
+def pedir_medio_transporte():
   transporte = ''
   while transporte != 'A' and transporte != 'T' and transporte != 'P' and transporte != 'X':
-    transporte = input('Ingresa el tipo de transporte: ')
-  return transporte 
+    transporte = input("Medio de transporte: ")
+  return transporte
 
-def ingresa_duracion():
+
+def pedir_ruta():
+  ruta = ''
+  while ruta != 'A' and ruta != 'B' and ruta != 'C' and ruta != 'O':
+    ruta = input("Ruta: ")
+  return ruta
+
+def pedir_tiempo():
   tiempo = -1
-  while tiempo<1:
+  while tiempo < 1:
     try:
-      tiempo = int(input('Ingresa la duracion del viaje: '))
-    except ValueError:
-      print('Error')
+      tiempo = int(input("Tiempo de viaje: "))
+    except:
+      print('error')
   return tiempo
 
-def ingresa_momento_dia():
+def pedir_momento ():
   momento = -1
-  while momento<1 or momento>4:
+  while momento < 1 or momento > 4:
     try:
-      momento = int(input('Ingresa el momento: '))
-    except ValueError:
-      print('Error')
+      momento = int(input("momento de viaje: "))
+    except:
+      print('error')
   return momento
 
-def ingresa_ruta():
-  ruta = ''
-  while ruta != 'A' and ruta != 'B'  and ruta != 'C' and ruta != 'O':
-    ruta = input('Ingresa la ruta: ')
-  return ruta 
-
-def guardar_datos():
+def registrar_datos():
   cont = 1
   while True:
-    transporte = ingresa_transporte()
-    if transporte == 'X':
+    medio_transporte = pedir_medio_transporte()
+    if medio_transporte == 'X':
       break
-    duracion = ingresa_duracion()
-    momento = ingresa_momento_dia()
-    ruta = ingresa_ruta()
-    diccionario[cont] = transporte,duracion,momento,ruta
-    cont+=1
+    ruta = pedir_ruta()
+    tiempo = pedir_tiempo()
+    momento = pedir_momento()
+    diccionario[cont] = medio_transporte, tiempo, momento,ruta
+    cont += 1
 
 
-def cantidad_medio_transporte():
-  lista_transporte = []
+
+def momentos_mayor_viaje():
+  momentos = list()
+  dict_contador = dict()
   for valor in diccionario.values():
-    lista_transporte.append(valor[0])
+      momentos.append(valor[2])
+  
+  for i in momentos:
+      dict_contador[i]=dict_contador.get(i,0)+1
+  mayor = max(dict_contador.values())
+  print('Momentos con mayor cantidad de viajes son:')
+  for clave,valor in dict_contador.items():
+    if valor == mayor:
+      print(clave,end=',')
+  print()
+      
+def cantidad_medio_transporte():
+  medios_transporte = []
+  for valor in diccionario.values():
+      medios_transporte.append(valor[0]) 
   print('Reporte')
   print('Cantidad de usuarios por medio de transporte')
-  print('Auto propio: ',lista_transporte.count('A'))
-  print('Privado: ',lista_transporte.count('T'))
-  print('Transporte público: ',lista_transporte.count('P'))
+  print('Auto propio: ',medios_transporte.count('A'))
+  print('Privado: ',medios_transporte.count('T'))
+  print('Transporte público: ',medios_transporte.count('P'))
 
 
-def momento_dia_mas_viajado():
-  lista_momento = []
-  lista_contadores = []
-  for valor in diccionario.values():
-    lista_momento.append(valor[2])
-  
-  for i in range(4):
-    lista_contadores.append(lista_momento.count(i+1))
-  mayor = max(lista_contadores)
-  print('Momentos con mayor cantidad de viajes son:')
-  for i in range(len(lista_contadores)):
-    if  lista_contadores[i] == mayor:
-      print(i+1,end=', ')
-  print()
-  
+
+
 def tiempo_promedio_ruta():
   contA = contB = contC = contO = 0
   sumaA = sumaB = sumaC = sumaO = 0
   for valor in diccionario.values():
     if valor[3] == 'A':
-      contA+=1
-      sumaA+=valor[1]
+      sumaA += valor[1]
+      contA += 1
     elif valor[3] == 'B':
-      contB+=1
-      sumaB+=valor[1]
+      sumaB += valor[1]
+      contB += 1
     elif valor[3] == 'C':
-      contC+=1
-      sumaC+=valor[1]
+      sumaC += valor[1]
+      contC += 1
     else:
-      contO+=1
-      sumaO+=valor[1]
+      sumaO += valor[1]
+      contO += 1
 
   if contA != 0:
     sumaA = sumaA/contA
@@ -93,15 +97,14 @@ def tiempo_promedio_ruta():
   if contO != 0:
     sumaO = sumaO/contO
 
-  print('Tiempo promedio de viaje por ruta son:')
-  print(f'Av. Arequipa: {sumaA}')
-  print(f'Av. Brasil: {sumaB}')
-  print(f'Paseo de la República: {sumaC}')
-  print(f'Otra ruta: {sumaO}')
-
+  print('Tiempo promedio por ruta')
+  print('Ruta A: ',sumaA)
+  print('Ruta B: ',sumaB)
+  print('Ruta C: ',sumaC)
+  print('Ruta O: ',sumaO)
 
 diccionario = dict()
-guardar_datos()
+registrar_datos()
 cantidad_medio_transporte()
-momento_dia_mas_viajado()
+momentos_mayor_viaje()
 tiempo_promedio_ruta()
