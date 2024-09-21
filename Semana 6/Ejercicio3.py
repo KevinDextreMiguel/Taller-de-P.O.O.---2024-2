f62f68a585a6aa9ce7841323fd6b924a22f1ccce
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2024-09-21 16:41:00 -0500

    Update Ejercicio3.py

--- Semana 6/Ejercicio3.py
+++ Semana 6/Ejercicio3.py
@@ -1,34 +1,33 @@
-from datetime import datetime
+from datetime import *
 
 def ingresar_fecha():
-  fecha = input("Ingrese la fecha de la primera cita YYYY/MM/DD: ")
-  fecha = datetime.strptime(fecha, "%Y/%m/%d")
+  fecha = input('Ingrese fecha: ')
+  formato = '%Y/%m/%d'
+  fecha = datetime.strptime(fecha,formato)
   return fecha
 
-def fechas_proxima_cita(fecha):
+
+def proximas_citas(fecha):
   dia = fecha.day
   mes = fecha.month
   anio = fecha.year
-  calendario = []
+  calendario = list()
 
-  if dia < 20:
-    mes = mes
-  else:
+  if dia > 20:
     mes = mes + 1
 
   for i in range(8):
     if mes > 12:
       mes = 1
-      anio = anio + 1
+      anio += 1
 
     if fecha.weekday() == 6:
       dia = 21
     else:
       dia = 20
-
-    fecha = datetime(anio, mes, dia)
+    
+    fecha = datetime(anio,mes,dia)
     calendario.append(fecha)
-
     mes += 1
 
   return calendario
@@ -37,6 +36,7 @@ def mostrar_calendario(calendario):
   for fecha in calendario:
     print(fecha.strftime("%Y/%m/%d"))
 
+#main
 fecha = ingresar_fecha()
-calendario = fechas_proxima_cita(fecha)
+calendario = proximas_citas(fecha)
 mostrar_calendario(calendario)
