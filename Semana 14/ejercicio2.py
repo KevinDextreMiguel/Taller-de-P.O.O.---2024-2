1a11452a307a805e1dfc151c894b15850a0bb714
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2024-11-16 16:42:42 -0500

    Update ejercicio2.py

--- Semana 14/ejercicio2.py
+++ Semana 14/ejercicio2.py
@@ -1,26 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def graficar(ventas,tipo):
-  titulo = 'Ventas primer trimestre'
-  graficos = {'lineas':'line','barras':'bar','circular':'pie','area':'area'}
-
-  if tipo == 'circular':
-    ventas.plot(kind=graficos[tipo],labels=ventas.index,autopct='%1.1f%%')
-  else:
-    fig,ax = plt.subplots()
-    ventas.plot(kind=graficos[tipo])
-    ax.set_ylabel('Nùmero de Ventas')
-    ax.set_xlabel('Año')
+class Ventas:
+  def __init__(self,dato):
+    self.__dato = dato
+    self.__titulo = 'Evolución de ventas'
+    self.__graficos = {'lineas':'line','barras':'bar','area':'area','pie':'pie'}
+  
+  def graficar(self,tipo):
+    if tipo == 'pie':
+      self.__dato.plot(kind=self.__graficos[tipo],autopct='%1.1f%%')
+    else:
+      fig,ax = plt.subplots()
+      self.__dato.plot(kind=self.__graficos[tipo])
+      ax.set_ylabel('Ventas')
+      ax.set_xlabel('Años')
+      ax.set_title(self.__titulo)
+    plt.show()
+    print()
     
-  plt.title(titulo)
-  plt.show()
-  print()
 
-ventas = [6578,6547,9876,5678]
 anios = [2021,2022,2023,2024]
-df_ventas = pd.Series(ventas,index=anios)
-graficar(df_ventas,'circular')
-graficar(df_ventas,'area')
-graficar(df_ventas,'barras')
-graficar(df_ventas,'lineas')
+ventas = [8976,8754,9871,7896]
+df = pd.Series(ventas,index=anios)
+
+objeto = Ventas(df)
+objeto.graficar('pie')
+objeto.graficar('barras')
+objeto.graficar('area')
+objeto.graficar('lineas')
