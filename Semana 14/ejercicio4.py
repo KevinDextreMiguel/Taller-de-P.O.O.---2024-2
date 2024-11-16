cf073882941ce5bfd40ee2c20f7dbf6f3917a6f7
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2024-11-16 16:43:25 -0500

    Update ejercicio4.py

--- Semana 14/ejercicio4.py
+++ Semana 14/ejercicio4.py
@@ -1,68 +1,78 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-def clasificar_edad(edad):
-  if edad<=18:
-    return 'Joven'
-  elif edad<=30:
-    return 'Joven Adulto'
-  elif edad<=50:
-    return 'Adulto'
-  elif edad<=70:
-    return 'Adulto Mayor'
-  else:
-    return 'Anciano'
+class Encuesta:
+  def __init__(self,n):
+    self.__edad = 0
+    self.__altura = 0
+    self.__peso = 0
+    self.__imc = 0
+    self.__sexo = ''
+    self.__clasificacion_edades = ''
+    self.__clasificacion_imc = ''
+    self.__tamanio = n
+    self.__datos = np.empty((self.__tamanio,7),dtype=object)
+  def calcula_imc(self):
+    self.__imc = self.__peso / (self.__altura**2)
+  def clasificar_edad(self):
+    if self.__edad <= 18:
+      self.__clasificacion_edades = 'Joven'
+    elif self.__edad <= 30:
+      self.__clasificacion_edades = 'Joven Adulto'
+    elif self.__edad <= 50:
+      self.__clasificacion_edades = 'Adulto'
+    elif self.__edad <= 70:
+      self.__clasificacion_edades = 'Adulto Mayor'
+    else:
+      self.__clasificacion_edades = 'Anciano'
+  def clasificar_imc(self):
+    if self.__imc <= 18.5:
+      self.__clasificacion_imc = 'Bajo'
+    elif self.__imc <= 24.9:
+      self.__clasificacion_imc = 'Medio Peso'
+    elif self.__imc <= 29:
+      self.__clasificacion_imc = 'Sobre Peso'
+    elif self.__imc <= 34.9:
+      self.__clasificacion_imc = 'Obeso bajo peso'
+    elif self.__imc <= 39.9:
+      self.__clasificacion_imc = 'Obeso Moderado'
+    elif self.__imc <= 49.9:
+      self.__clasificacion_imc = 'Obeso Mórbido'
+    else:
+      self.__clasificacion_imc = 'Obeso Extremo'
+  def generar(self):
+    for i in range(self.__tamanio):
+      self.__edad = np.random.randint(0,300)
+      self.__altura = np.random.randint(100,200)/100
+      self.__peso = np.random.randint(1,200)
+      self.__sexo = np.random.choice(['M','F'])
+      self.calcula_imc()
+      self.clasificar_edad()
+      self.clasificar_imc()
 
-def clasificar_obesidad(imc):
-  valor = 'Obeso Extremo'
-  if imc<=18.5:
-    valor = 'Bajo'
-  elif imc<=24.9:
-    valor = 'Medio Peso'
-  elif imc<=29:
-    valor = 'Sobrepeso'
-  elif imc<=34.9:
-    valor = 'Obeso bajo peso '
-  elif imc<=39.9:
-    valor = 'Obeso Moderado'
-  elif imc<=49.9:
-    valor = 'Obeso Mórbido'
-  return valor
-
-def graficar(df,columna,titulo):
-  cantidad = df[columna].value_counts()
-  cantidad.plot(kind='pie',title=titulo,autopct='%1.1f%%')
-  plt.show()
-  print()
-
-def estadisticas(df):
-  print(f'Peso máximo {df["Peso"].max():.2f}')
-  print(f'Peso mínimo {df["Peso"].min():.2f}')
-  print(f'Peso promedio {df["Peso"].mean():.2f}')
-
-def generar(cantidad):
-  datos = np.empty((cantidad,7),dtype=object)
-  for i in range(cantidad):
-    decimal = np.random.rand()
-    edad = np.random.randint(0,200)
-    altura = np.random.randint(1,3)*decimal
-    peso = np.random.randint(1,200)*decimal
-    sexo = np.random.choice(['Hombre','Mujer'])
-    imc = peso/(altura**2)
-    clasificacion_edad = clasificar_edad(edad)
-    clasificar_imc = clasificar_obesidad(imc)
+      self.__datos[i] = [self.__edad,self.__altura,self.__peso,self.__imc,self.__sexo,self.__clasificacion_edades,self.__clasificacion_imc]
     
-    datos[i] = [edad,altura,peso,sexo,imc,clasificacion_edad,clasificar_imc]
-  #convertir a pandas
-  columnas = ['Edad','Altura','Peso','Sexo','IMC','Clasificación Edad','Clasificación IMC']
-  df = pd.DataFrame(datos,columns=columnas)
+    columnas = ['Edad','Altura','Peso','IMC','Sexo','Clasificación de edad','Clasificación de IMC']
+    df = pd.DataFrame(self.__datos,columns=columnas)
+    print(df)
+    self.graficar(df,'Clasificación de edad','Gràfico')
+    self.graficar(df,'Clasificación de IMC','Gràfico')
+    self.estadistica(df,'Peso')
+
+  def graficar(selfdf,df,columna,titulo):
+    cantidad = df[columna].value_counts()
+    cantidad.plot(kind='pie',autopct='%1.1f%%')
+    plt.title(titulo)
+    plt.show()
+    print()
   
-  graficar(df,'Clasificación Edad','Clasificación por Edad')
-  graficar(df,'Clasificación IMC','Clasificación por IMC')
+  def estadistica(self,df,columna):
+    print(f'Peso promedio: {df[columna].mean()}')
+    print(f'Peso mínimo: {df[columna].min()}')
+    print(f'Peso máximo: {df[columna].max()}')
 
-  print(df)
-  estadisticas(df)
-    
-n = 10
-generar(n)
+
+n = int(input('N: '))
+objeto = Encuesta(n)
+objeto.generar()
