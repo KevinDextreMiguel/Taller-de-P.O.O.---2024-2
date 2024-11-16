81224aadb3cbecbc766e00321eba412bcfb32c22
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2024-11-16 16:43:02 -0500

    Update ejercicio3.py

--- Semana 14/ejercicio3.py
+++ Semana 14/ejercicio3.py
@@ -1,21 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def generar_datos():
-  dato = {'Meses':['Ene','Feb','Mar','Abr'],
-          'Ventas':[6578,8765,9876,9854],
-          'Gastos':[4567,4398,5456,6578]}
-  dato = pd.DataFrame(dato)
-  return dato
+class Ventas:
+  def __init__(self,datos):
+    self.__datos = datos
+    self.__titulo = 'Evolución de ingresos y gastos'
+  def graficar(self):
+    fig,ax = plt.subplots()
+    self.__datos.plot(kind='line',ax=ax)
+    ax.set_ylabel('Ingresos y gastos')
+    ax.set_xlabel('Años')
+    plt.grid(True)
+    ax.set_title(self.__titulo)
+    plt.show()
 
-def graficar(datos):
-  titulo = 'Evolución de ingresos y gastos'
-  fig,ax = plt.subplots()
-  plt.title(titulo)
-  datos.plot(kind='line',x='Meses',y='Ventas',ax=ax,color='yellow')
-  datos.plot(kind='line',x='Meses',y='Gastos',color='red',ax=ax)
-  plt.grid(True)
-  plt.show()
+datos = {'Meses':['Ene','Feb','Mar','Abr'],
+         'Gatos':[8976,7456,8789,8345],
+         'Ingresos':[9976,8456,9789,8345]}
+df = pd.DataFrame(datos).set_index('Meses')
 
-dato = generar_datos()
-graficar(dato)
+#crear un objeto
+objeto = Ventas(df)
+objeto.graficar()
