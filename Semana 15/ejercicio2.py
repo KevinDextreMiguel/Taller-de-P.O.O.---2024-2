9a0cb145e5cc741960d16da2165ecf3c20e80558
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2024-11-23 16:46:04 -0500

    Update ejercicio2.py

--- Semana 15/ejercicio2.py
+++ Semana 15/ejercicio2.py
@@ -1,55 +1,58 @@
 import pandas as pd
 import numpy as np
 
-class Data:
+class Datos:
   def __init__(self):
     self.__df = None
     self.__copia = None
   def leer(self):
     self.__df = pd.read_csv('iris.csv',index_col=0)
+    #print(self.__df)
+  def poner_encabezados(self):
+    names = ('sepallength', 'sepalwidth', 'petallength', 'petalwidth', 'species') 
+    self.__df.columns = names
     print(self.__df)
-  def poner_nombre_columnas(self):
-    nombres = ['sepallength', 'sepalwidth', 'petallength', 'petalwidth', 'species']
-    self.__df.columns = nombres
-    print(self.__df)
-  def extraer_sexta_columna(self):
-    specie = self.__df['species']
-    print(specie)
+  def extraer_6columna(self):
+    columna = self.__df.species #self.__df['species']
+    print('información columna species')
+    print(columna)
   def estadistica(self):
-    media = self.__df['sepallength'].mean()
+    media = self.__df.sepallength.mean()
     mediana = self.__df['sepallength'].median()
-    desviacion_estandar = self.__df['sepallength'].std()
-    print(f"Media columna sepallength: {media}")
-    print(f"Mediana columna sepallength: {mediana}")
-    print(f"Desviación estándar columna sepallength: {desviacion_estandar}")
-  def insertar_nan(self):
+    desviacion = self.__df.sepallength.std()
+    print(f'media: {media}')
+    print(f'mediana: {mediana}')
+    print(f'desviacion: {desviacion}')
+  def crear_copia(self):
     cantidad = 30
     self.__copia = self.__df.copy()
-    indices_nan = np.random.choice(self.__copia.index, size=cantidad, replace=False)
-    columnas_nan = np.random.choice(self.__copia.columns, size=cantidad,replace=True)
+    indices_nan = np.random.choice(self.__copia.index, cantidad, replace=False)
+    columnas_nan = np.random.choice(self.__copia.columns, cantidad, replace=True)
     for i in range(cantidad):
-      self.__copia.loc[indices_nan[i],columnas_nan[i]] = np.nan
+      self.__copia.loc[indices_nan[i], columnas_nan[i]] = np.nan
     print(self.__copia)
   def nan_segunda_columna(self):
-    nan_valores = self.__copia['sepalwidth'].isna()
-    cantidad_nan = nan_valores.sum()
-    posiciones_nan = self.__copia[nan_valores].index.tolist()
-    print(f"Cantidad de valores nan en la segunda columna: {cantidad_nan}")
-    print(f"Posiciones de los valores nan en la segunda columna: {posiciones_nan}")
+    nan_sepalwidth = self.__copia['sepalwidth'].isna()
+    cantidad = nan_sepalwidth.sum()
+    indices = self.__copia[nan_sepalwidth].index.tolist()
+    print(f'Cantidad de valores nan en la segunda columna: {cantidad}')
+    print(f'Indices de los valores nan en la segunda columna: {indices}')
   def filas_sin_nan(self):
     filas_sin_nan = self.__copia.dropna()
     print(filas_sin_nan)
-  def filtrar_condicion(self):
-    filtra = self.__df[(self.__df['sepallength'] < 5) & (self.__df['petallength'] > 1.5)]
-    print('filtrar')
-    print(filtra)
+  def filtrar(self):
+    data = self.__copia[(self.__copia['sepallength']<5.0) & (self.__copia['petallength']>1.5)]
+    print('filtro')
+    print(data)
 
-objeto = Data()
+    
+
+objeto = Datos()
 objeto.leer()
-objeto.poner_nombre_columnas()
-objeto.extraer_sexta_columna()
+objeto.poner_encabezados()
+objeto.extraer_6columna()
 objeto.estadistica()
-objeto.insertar_nan()
+objeto.crear_copia()
 objeto.nan_segunda_columna()
 objeto.filas_sin_nan()
-objeto.filtrar_condicion()
+objeto.filtrar()
